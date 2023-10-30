230073caad43c035f497d863a4727e4c358029fd
ikrost <arthurlucasbd@gmail.com>
2023-10-30 18:40:59 -0300

    added function isUserExists

--- api/controllers/userController.ts
+++ api/controllers/userController.ts
@@ -7,13 +7,10 @@ export let Users = [
 export function getUser(req: Request, res: Response) {
 
     const id: number = parseInt(req.params.id)
+    isUserExists(id, res)
 
     const user = Users.find(user => user.id === id);
-    if (user) {
-        res.send(user);
-    } else {
-        res.status(404).send('User not found');
-    }
+    res.send(user);
 
 }
 
@@ -35,13 +32,14 @@ export function addUser(req: Request, res: Response) {
 export function updateUser(req: Request, res: Response) {
 
     const id: number = parseInt(req.params.id)
+    isUserExists(id, res)
     const [data] = req.body
 
     const user: any = Users.find(user => user.id === id)
     for (const property in data) {
         user[property] = data[property];
     } 
-    
+
     res.status(200).send('OK')
 
 }
@@ -49,10 +47,18 @@ export function updateUser(req: Request, res: Response) {
 export function deleteUser(req: Request, res: Response) {
 
     const id: number = parseInt(req.params.id)
+    isUserExists(id, res)
 
     const updatedUsers = Users.filter(user => user.id !== id);
     Users = updatedUsers;
         
     res.status(200).send('OK')
 
+}
+
+function isUserExists(id: number, res: Response) {
+    const user: any = Users.find(user => user.id === id)
+    if (!user) {
+        return res.status(404).send('User not found');
+    }
 }
